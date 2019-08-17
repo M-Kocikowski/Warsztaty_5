7ae5494763a8681da8903f88cf193fafab50616d
M-Kocikowski <marcin.kocikowski@gmail.com>
2019-08-17 13:00:40 +0200

    Project setup & pom.xml

---
+++ src/main/java/pl/coderslab/bookapi/AppConfig.java
@@ -0,0 +1,13 @@
+package pl.coderslab.bookapi;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan
+public class AppConfig extends WebMvcConfigurerAdapter {
+
+}
